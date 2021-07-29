138b90f009c2d69d3cdec3754fd81560bdd3a73f
Alyssa DiCarlo <alyssa.dicarlo@gmail.com>
2021-07-29 11:44:44 -0700

    initial commit

---
+++ src/containers/OutputContainer.jsx
@@ -0,0 +1,11 @@
+import Output from './../components/Output';
+import { connect } from 'react-redux';
+
+function mapStateToProps(state) {
+
+    return {
+        amount: state.count
+    }
+}
+
+export default connect(mapStateToProps)(Output);
